3dbd4886d307130cb3dcb62661c7febce2c8b952
AbdulRehman228 <syedabdulrahman996@gmail.com>
2025-04-15 23:01:42 +0500

    second comment

--- Assignments_00_07/00_intro_python/03_fahrenheit_to_celsius.py
+++ Assignments_00_07/00_intro_python/03_fahrenheit_to_celsius.py
@@ -1,5 +1,11 @@
 def main():
 
-    fahrenheit = float(input("\033[1;3mEnter temperature in Fahrenheit: \033[0m"))
+    fahrenheit = float(input("\033[1;3m Enter temprature in fahrenheit: \033[0m"))
 
-    celsius = (fahrenheit - 32) * 5 / 9
+    celsius = (fahrenheit - 32) * 5.0/9.0
+
+    print(f"temprature: {fahrenheit}F = {celsius}C")
+
+
+if __name__ == '__main__':
+    main()
